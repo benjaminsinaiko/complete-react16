9c930c0bb6338a2df2c6b338a61c9629825ab7e1
benjaminsinaiko <ben.jamin@outlook.com>
2018-06-07 00:21:37 -0500

    Changing state and props with event handlers

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,18 +1,50 @@
-import React, { Component } from 'react';
-import './App.css';
-import Person from './Person/Person';
+import React, { Component } from 'react'
+import './App.css'
+import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 26 }
+    ],
+    otherState: 'some other value'
+  }
+
+  switchNameHandler = () => {
+    // console.log('Was clicked!')
+    this.setState({
+      persons: [
+        { name: 'Maximilian', age: 28 },
+        { name: 'Manu', age: 29 },
+        { name: 'Stephanie', age: 27 }
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working</p>
-        <Person name="Max" age="28" />
-        <Person name="Manu" age="29">My Hobbies: Horse racing</Person>
-        <Person name="Stephanie" age="26" />
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        >
+          My Hobbies: Horse racing
+        </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
       </div>
-    );
+    )
     // return React.createElement(
     //   'div',
     //   { className: 'App' },
@@ -21,4 +53,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default App
