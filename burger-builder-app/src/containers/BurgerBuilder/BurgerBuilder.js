028da9219d014ea7f89056d7be63a71f82face69
benjaminsinaiko <ben.jamin@outlook.com>
2018-09-24 16:39:31 -0500

    Add routing and set up navigation to checkout page

--- burger-builder-app/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -94,27 +94,29 @@ class BurgerBuilder extends Component {
 
   purchaseContinueHandler = () => {
     // alert('You continue!');
-    this.setState({ loading: true });
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: 'Ben',
-        address: {
-          street: 'Teststreet 1',
-          zipCode: '60610',
-          country: 'USA'
-        },
-        email: 'test@email.com'
-      },
-      deliveryMethod: 'fastest'
-    };
-    axios
-      .post('/orders.json', order)
-      .then(response => {
-        this.setState({ loading: false, purchasing: false });
-      })
-      .catch(error => this.setState({ loading: false, purchasing: false }));
+    // this.setState({ loading: true });
+    // const order = {
+    //   ingredients: this.state.ingredients,
+    //   price: this.state.totalPrice,
+    //   customer: {
+    //     name: 'Ben',
+    //     address: {
+    //       street: 'Teststreet 1',
+    //       zipCode: '60610',
+    //       country: 'USA'
+    //     },
+    //     email: 'test@email.com'
+    //   },
+    //   deliveryMethod: 'fastest'
+    // };
+    // axios
+    //   .post('/orders.json', order)
+    //   .then(response => {
+    //     this.setState({ loading: false, purchasing: false });
+    //   })
+    //   .catch(error => this.setState({ loading: false, purchasing: false }));
+
+    this.props.history.push('/checkout');
   };
 
   render() {
