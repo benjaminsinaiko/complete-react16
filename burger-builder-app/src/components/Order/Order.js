5e8bfe75112aca0f2eac0f7b04aec544788ad6f0
benjaminsinaiko <ben.jamin@outlook.com>
2018-09-25 00:56:28 -0500

    Add orders page

---
+++ burger-builder-app/src/components/Order/Order.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import styles from './Order.module.css';
+
+const order = props => (
+  <div className={styles.Order}>
+    <p>Ingredients: Lettuce (1)</p>
+    <p>
+      Price: <strong>USD 5.45</strong>
+    </p>
+  </div>
+);
+
+export default order;
