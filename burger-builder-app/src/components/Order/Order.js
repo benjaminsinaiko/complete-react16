d3163c3706c00a6d98725bbe09ce201107dd6fb2
benjaminsinaiko <ben.jamin@outlook.com>
2018-09-26 01:32:16 -0500

    Output orders from firebase, transform to display

--- burger-builder-app/src/components/Order/Order.js
+++ burger-builder-app/src/components/Order/Order.js
@@ -2,13 +2,31 @@ import React from 'react';
 
 import styles from './Order.module.css';
 
-const order = props => (
-  <div className={styles.Order}>
-    <p>Ingredients: Lettuce (1)</p>
-    <p>
-      Price: <strong>USD 5.45</strong>
-    </p>
-  </div>
-);
+const order = props => {
+  const ingredients = [];
+  for (let ingredientName in props.ingredients) {
+    ingredients.push({
+      name: ingredientName,
+      amount: props.ingredients[ingredientName]
+    });
+  }
+
+  const ingredientOutput = ingredients.map(ig => {
+    return (
+      <span key={ig.name} className={styles.Ingredients}>
+        {ig.name} ({ig.amount})
+      </span>
+    );
+  });
+
+  return (
+    <div className={styles.Order}>
+      <p>{ingredientOutput}</p>
+      <p>
+        Price: <strong>USD {props.price.toFixed(2)}</strong>
+      </p>
+    </div>
+  );
+};
 
 export default order;
