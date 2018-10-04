eaf7ac7f38ca25dc65f4b568206f8b745747e825
benjaminsinaiko <ben.jamin@outlook.com>
2018-10-04 00:07:13 -0500

    Restructure actions files, split up and add index

--- redux--01-start/src/containers/Counter/Counter.js
+++ redux--01-start/src/containers/Counter/Counter.js
@@ -1,13 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import {
-  increment,
-  decrement,
-  add,
-  subtract,
-  storeResult,
-  deleteResult
-} from '../../store/actions/actions';
+import * as actionCreators from '../../store/actions/index';
 
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
@@ -58,12 +51,12 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
   return {
-    onIncrementCounter: () => dispatch(increment()),
-    onDecrementCounter: () => dispatch(decrement()),
-    onAddCounter: () => dispatch(add(5)),
-    onSubtractCounter: () => dispatch(subtract(5)),
-    onStoreResult: result => dispatch(storeResult(result)),
-    onDeleteResult: id => dispatch(deleteResult(id))
+    onIncrementCounter: () => dispatch(actionCreators.increment()),
+    onDecrementCounter: () => dispatch(actionCreators.decrement()),
+    onAddCounter: () => dispatch(actionCreators.add(5)),
+    onSubtractCounter: () => dispatch(actionCreators.subtract(5)),
+    onStoreResult: result => dispatch(actionCreators.storeResult(result)),
+    onDeleteResult: id => dispatch(actionCreators.deleteResult(id))
   };
 };
 
--- redux--01-start/src/store/reducers/result.js
+++ redux--01-start/src/store/reducers/result.js
@@ -1,4 +1,4 @@
-import * as actionTypes from '../actions/actions';
+import * as actionTypes from '../actions/actionTypes';
 
 const initialState = {
   results: []
@@ -6,22 +6,22 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-  case actionTypes.STORE_RESULT:
-    return {
-      ...state,
-      results: state.results.concat({
-        id: new Date(),
-        value: action.result
-      })
-    };
-  case actionTypes.DELETE_RESULT: {
-    const updatedArray = state.results.filter(
-      result => result.id !== action.resultId
-    );
-    return { ...state, results: updatedArray };
-  }
-  default:
-    return state;
+    case actionTypes.STORE_RESULT:
+      return {
+        ...state,
+        results: state.results.concat({
+          id: new Date(),
+          value: action.result
+        })
+      };
+    case actionTypes.DELETE_RESULT: {
+      const updatedArray = state.results.filter(
+        result => result.id !== action.resultId
+      );
+      return { ...state, results: updatedArray };
+    }
+    default:
+      return state;
   }
 };
 
