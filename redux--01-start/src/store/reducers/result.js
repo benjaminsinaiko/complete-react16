31b4a7a5e04589d1e753d3f89d20901ef485a86e
benjaminsinaiko <ben.jamin@outlook.com>
2018-10-04 00:40:58 -0500

    Add utility function to clean up reducers

--- redux--01-start/src/store/reducers/result.js
+++ redux--01-start/src/store/reducers/result.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
 
 const initialState = {
   results: []
@@ -6,22 +7,21 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case actionTypes.STORE_RESULT:
-      return {
-        ...state,
-        results: state.results.concat({
-          id: new Date(),
-          value: action.result
-        })
-      };
-    case actionTypes.DELETE_RESULT: {
-      const updatedArray = state.results.filter(
-        result => result.id !== action.resultId
-      );
-      return { ...state, results: updatedArray };
-    }
-    default:
-      return state;
+  case actionTypes.STORE_RESULT:
+    return updateObject(state, {
+      results: state.results.concat({
+        id: new Date(),
+        value: action.result
+      })
+    });
+  case actionTypes.DELETE_RESULT: {
+    const updatedArray = state.results.filter(
+      result => result.id !== action.resultId
+    );
+    return updateObject(state, { results: updatedArray });
+  }
+  default:
+    return state;
   }
 };
 
