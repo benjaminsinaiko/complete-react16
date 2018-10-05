1463cd329d2739e357b6596c5f74700506440c5b
benjaminsinaiko <ben.jamin@outlook.com>
2018-10-05 00:21:11 -0500

    Fetch orders with redux

--- burger-builder-app/src/store/actions/order.js
+++ burger-builder-app/src/store/actions/order.js
@@ -28,7 +28,6 @@ export const purchaseBurger = orderData => {
     axios
       .post('/orders.json', orderData)
       .then(response => {
-        console.log(response.data);
         dispatch(purchaseBurgerSuccess(response.data.name, orderData));
       })
       .catch(error => {
@@ -40,5 +39,43 @@ export const purchaseBurger = orderData => {
 export const purchaseInit = () => {
   return {
     type: actionTypes.PURCHASE_INIT
+  };
+};
+
+export const fetchOrderSuccess = orders => {
+  return {
+    type: actionTypes.FETCH_ORDERS_SUCCESS,
+    orders: orders
+  };
+};
+
+export const fetchOrdersFail = error => {
+  return {
+    type: actionTypes.FETCH_ORDERS_FAIL,
+    error: error
+  };
+};
+
+export const fetchOrdersStart = () => {
+  return {
+    type: actionTypes.FETCH_ORDERS_START
+  };
+};
+
+export const fetchOrders = () => {
+  return dispatch => {
+    dispatch(fetchOrdersStart());
+    axios
+      .get('/orders.json')
+      .then(res => {
+        const fetchedOrders = [];
+        for (let key in res.data) {
+          fetchedOrders.push({ ...res.data[key], id: key });
+        }
+        dispatch(fetchOrderSuccess(fetchedOrders));
+      })
+      .catch(error => {
+        dispatch(fetchOrdersFail(error));
+      });
   };
 };
