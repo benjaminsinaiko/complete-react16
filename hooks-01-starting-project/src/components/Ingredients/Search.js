ffbb867d0b0c759916303ed5a920e8201d602aec
benjaminsinaiko <ben.jamin@outlook.com>
2019-12-04 17:17:26 -0600

    Add custom http hook to search component

--- hooks-01-starting-project/src/components/Ingredients/Search.js
+++ hooks-01-starting-project/src/components/Ingredients/Search.js
@@ -1,40 +1,47 @@
 import React, { useState, useEffect, useRef } from 'react';
 
 import Card from '../UI/Card';
+import ErrorModal from '../UI/ErrorModal';
+import useHttp from '../../hooks/http';
 import './Search.css';
 
 const Search = React.memo(props => {
   const { onLoadIngredients } = props;
   const [enteredFilter, setEnteredFilter] = useState('');
   const inputRef = useRef();
+  const { isLoading, data, error, sendRequest, clearError } = useHttp();
 
   useEffect(() => {
     const timer = setTimeout(() => {
       if (enteredFilter === inputRef.current.value) {
         const query =
           enteredFilter.length === 0 ? '' : `?orderBy="title"&equalTo="${enteredFilter}"`;
-        fetch(process.env.REACT_APP_DB_URL + '.json' + query)
-          .then(response => response.json())
-          .then(responseData => {
-            const loadedIngredients = Object.entries(responseData).map(([key, value]) => ({
-              id: key,
-              title: value.title,
-              amount: value.amount
-            }));
-            onLoadIngredients(loadedIngredients);
-          });
+        sendRequest(process.env.REACT_APP_DB_URL + '.json' + query, 'GET');
       }
     }, 600);
     return () => {
       clearTimeout(timer);
     };
-  }, [enteredFilter, onLoadIngredients, inputRef]);
+  }, [enteredFilter, sendRequest, inputRef]);
+
+  useEffect(() => {
+    if (!isLoading && !error && data) {
+      const loadedIngredients = Object.entries(data).map(([key, value]) => ({
+        id: key,
+        title: value.title,
+        amount: value.amount
+      }));
+      onLoadIngredients(loadedIngredients);
+    }
+  }, [data, isLoading, error, onLoadIngredients]);
 
   return (
     <section className='search'>
+      {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
       <Card>
         <div className='search-input'>
           <label>Filter by Title</label>
+          {isLoading && <span>Loading...</span>}
           <input
             ref={inputRef}
             type='text'
